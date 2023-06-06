fe50f32affa9c1ddd49da72e12b91260871a35a0
begimai <begimajmamatkulova7@gmail.com>
2023-06-06 20:20:30 +0600

    inline_button, special_button

--- bot.py
+++ bot.py
@@ -5,7 +5,9 @@ from aiogram import Bot, Dispatcher, types
 from config import TOKEN
 from aiogram.filters.command import Command
 from aiogram.enums.dice_emoji import DiceEmoji
-
+from aiogram.types import ReplyKeyboardMarkup
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+from aiogram.types import KeyboardButton
 
 logging.basicConfig(level=logging.INFO)
 
@@ -30,12 +32,59 @@ async def dice(message: types.Message):
 async def football(message: types.Message):
     await message.answer_dice(emoji=DiceEmoji.FOOTBALL)
     
-@dp.message()
-async def text(message: types.Message):
-    await message.answer(message.text)    
+@dp.message(Command("special_button"))
+async def special_button(message: types.Message):
+    builder = ReplyKeyboardBuilder()
+    builder.row(
+        types.KeyboardButton(text="Отпарвка номера", request_contact = True),
+        types.KeyboardButton(text="Отпарвка геолокации", request_location=True),
+    )
+    builder.row(types.KeyboardButton(
+        text="Создать викторину", 
+        request_poll = types.KeyboardButtonPollType(type="quiz")
+    ))
+       
+    await message.answer(
+        "Выберите действие: ",
+        reply_markup=builder.as_markup(resize_keyboard = True)
+    )
+
+@dp.message(Command("inline_button"))
+async def inline_button(message: types.Message):
+    builder = InlineKeyboardBuilder()
+    builder.row(types.InlineKeyboardButton(
+        text="GitHub", url="https://github.com/"
+    ))
+    builder.row(types.InlineKeyboardButton(
+        text="Telegram",
+        url="tg://resolve?domain=telegram"
+    ))
+    
+    user_id = 1312702673
+    chat_info = await bot.get_chat(user_id)
+    if not chat_info.has_private_forwards:
+        builder.row(types.InlineKeyboardButton(
+            text="Какой-то пользователь",
+            url=f"tg://user?id={user_id}"
+        ))
+    
+    await message.answer(
+        "Выберите ссылку",
+        reply_markup=builder.as_markup(),
+    )
+    
+# @dp.message()
+# async def text(message: types.Message):
+#     await message.answer(message.text)  
+    
+@dp.message(Command('id'))
+async def id(message: types.Message):   
+    await message.answer(message.from_user.id)       
 
 async def main():
     await dp.start_polling(bot)
     
 if __name__ == "__main__":
     asyncio.run(main())
+    
+    
